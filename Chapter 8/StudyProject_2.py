1b9d48dead2c36cc314397f99ca83f03974d8b86
dench111 <46183310+dench111@users.noreply.github.com>
2020-02-02 15:50:42 +0300

    рабочий вариант

--- Chapter 8/StudyProject_2.py
+++ Chapter 8/StudyProject_2.py
@@ -2,10 +2,22 @@
 #  Напишите программу, которая обходит дерево папок, выполняя поиск исключительно больших
 #  по своим размерам файлов и папок, размеры которых > 100 Мб. Выведите абсолютные пути доступа к этим файлам на экран
 
-import shutil, os, StProj_2_Folders.py
+import shutil, os, StProj_2_Folders
 
 searchDir = StProj_2_Folders.params['Директория с файлами для поиска']
-print(searchDir)
+print('Директория с файлами для поиска ' + searchDir)
 
-fileSearchSize = StProj_2_Folders.params['Искать файлы с размером >=']
-print(fileSearchSize)
+fileSearchSize = StProj_2_Folders.params['Искать файлы с размером Мб >=']
+fileSizeinBytes = int(fileSearchSize) * 2 ** 20
+print('Искать файлы с размером >= ' + str(fileSizeinBytes) + ' байт')
+
+for foldername, subfolders, filenames in os.walk(searchDir):
+    for subfolder in subfolders:
+        print('')
+    for filename in filenames:
+        location = foldername + '\\' + filename  # определение полного пути + названия файла
+        # print(location)
+        if os.path.getsize(location) >= fileSizeinBytes:
+            fileSizeinMbytes = os.path.getsize(location) // (2**20)
+            print('Найден файл: ' + location + ' с размером: ' + str(os.path.getsize(location)) +
+                  ' байт или ' + str(fileSizeinMbytes) + ' Мбайта')
